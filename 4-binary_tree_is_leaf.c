d7faa595bc52c032e0d759a4411c1ea706b07fe5
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-01-05 01:20:00 +0200

    alx

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - function that checks if a node is a leaf
- * @node: node to  check
- * Return: 1 if is leaf or 0 if not
+ * binary_tree_is_leaf - function that check
+ * @node: node
+ * Return: 1 or 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
