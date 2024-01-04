7c8e91f83ff117de4f7b9225f8710272a2d63c81
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-01-05 01:20:24 +0200

    Update 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - function that checks if a node is root
- * @node: node to  check
- * Return: 1 if is root or 0 if not
+ * binary_tree_is_root - function that checks
+ * @node: node
+ * Return: 1 or 0
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
